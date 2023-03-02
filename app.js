a56baac527bbb04444f61430121e4a8c68e79a15
BokkenKun <fransantogores@gmail.com>
2023-03-02 15:36:14 +0100

    Basic CRUD Functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverrride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://127.0.0.1:27017/tucamping')
@@ -18,10 +20,12 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverrride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -32,9 +36,36 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } =req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } =req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
