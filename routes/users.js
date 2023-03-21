2447bb156c5afbc6b5e0de142507d87b2ace3f7c
BokkenKun <fransantogores@gmail.com>
2023-03-21 17:53:36 +0100

    fixing register route, returnTo behaviour

--- routes/users.js
+++ routes/users.js
@@ -28,15 +28,19 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 });
 
-router.get('/logout', (req, res) => {
-    req.logout();
+router.get('/logout', function(req, res, next){
+    req.logout(function(err) {
+    if (err) { return next(err); }
     req.flash('success', 'Goodbye!');
     res.redirect('/campgrounds');
+    });
 });
 
 module.exports = router;
