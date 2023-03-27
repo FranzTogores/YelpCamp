b91ccf00a2297c8ad314dde7a9285050a308aa6a
BokkenKun <fransantogores@gmail.com>
2023-03-27 18:43:33 +0200

    bug fixing and map popup

--- seeds/index.js
+++ seeds/index.js
@@ -27,11 +27,16 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20 + 10);
         const camp = new Campground({
+            //YOUR USER ID
             author: '6419925e9f95eb31554657f6',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste magni quis, dolore inventore dolorum debitis, voluptates, eaque aspernatur fuga praesentium eligendi numquam? Ipsum ex quaerat accusantium sapiente earum saepe tempore.',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [-133.1331, 47.0202]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dsm2itjc5/image/upload/v1679740509/YelpCamp/wzbmpoek3wyv79igm2u9.jpg',
