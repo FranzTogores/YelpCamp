9eb70c9bcbda9f5a67fd422c65eddfdb080de168
BokkenKun <fransantogores@gmail.com>
2023-03-30 16:03:38 +0200

    app use

--- app.js
+++ app.js
@@ -70,9 +70,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    if (!['/login', '/'].includes(req.originalUrl)) {
-        req.session.returTo = req.originalUrl;
-    }
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
